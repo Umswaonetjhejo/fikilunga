fcafe07d69a8303c2c9e7378b54aa7623001c7dc
Musa Skosana <mskosana@twkagri.com>
2019-04-01 21:10:22 +0200

    Fiki Lunga firt commit

---
+++ index.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="">
+<title>Fiki Lunga Properties</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<style>
+
+    img#mySlides{
+        width: 100%;
+        height: 700px;
+    }
+
+    img#navLogo{
+        height: 54px;
+        width: 245px;
+        margin: -8px;;
+    }
+
+    #developer{
+        text-align: center;
+        color: dodgerblue;
+        font-size: 13px;
+    }
+</style>
+<body>
+
+<!-- Navbar (sit on top) -->
+<div class="w3-top">
+  <div class="w3-bar w3-white w3-wide w3-padding w3-card">
+    <a href="#home" class="w3-bar-item w3-button">
+        <img class="w3-image" src="img/logo/logo2.png" alt="Logo" id="navLogo">
+    </a>
+    <!-- Float links to the right. Hide them on small screens -->
+    <div class="w3-right w3-hide-small">
+      <a href="#home" class="w3-bar-item w3-button">Home</a>
+      <a href="#property" class="w3-bar-item w3-button">Property</a>
+      <a href="#about" class="w3-bar-item w3-button">About</a>
+      <a href="#agents" class="w3-bar-item w3-button">Agents</a>
+      <a href="#contact" class="w3-bar-item w3-button">Contact</a>
+    </div>
+  </div>
+</div>
+
+<!-- Header -->
+<header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
+
+<!--  <img class="w3-image" src="img/slides/slide1.jpg" alt="house" width="1500" height="100">-->
+
+    <img class="mySlides" src="img/slides/slide1.jpg" alt="slide1" id="mySlides">
+    <img class="w3-image mySlides" src="img/slides/slide2.jpg" alt="slide2" id="mySlides">
+    <img class="w3-image mySlides" src="img/slides/slide3.jpg" alt="slide3" id="mySlides">
+    <img class="w3-image mySlides" src="img/slides/slide4.jpg" alt="slide4" id="mySlides">
+    <img class="w3-image mySlides" src="img/slides/slide5.jpg" alt="slide5" id="mySlides">
+
+  <div class="w3-display-middle w3-margin-top w3-center">
+    <h1 class="w3-xxlarge w3-text-white">
+        <img class="w3-image" src="img/logo/logo2.png" alt="Logo" id="navLogoHome">
+    </h1>
+  </div>
+</header>
+
+<!-- Page content -->
+<div class="w3-content w3-padding" style="max-width:1564px">
+
+  <!-- Project Section -->
+  <div class="w3-container w3-padding-32" id="property">
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Property</h3>
+  </div>
+
+  <div class="w3-row-padding">
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container w3-card">
+        <div class="w3-display-topleft w3-black w3-padding">R3.6 million</div>
+        <img src="img/property/Bushwillow Park Estate, Greenstone Hill R3.6million/pic1.jpg" alt="House" style="width:100%">
+          <div class="w3-container">
+              <p>Bushwillow Park Estate, Greenstone Hill</p>
+          </div>
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container w3-card">
+        <div class="w3-display-topleft w3-black w3-padding">R680 000</div>
+        <img src="img/property/Dawn Park R680 000/pic1.jpeg" alt="House" style="width:100%">
+          <div class="w3-container">
+              <p>Dawn Park</p>
+          </div>
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container w3-card">
+        <div class="w3-display-topleft w3-black w3-padding">R760 000</div>
+        <img src="img/property/Dawn Park R760 000/pic1.jpg" alt="House" style="width:100%">
+          <div class="w3-container">
+              <p>Dawn Park</p>
+          </div>
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container w3-card">
+        <div class="w3-display-topleft w3-black w3-padding">R980 000</div>
+        <img src="img/property/Dawn Park R980k/pic1.jpeg" alt="House" style="width:100%">
+          <div class="w3-container">
+              <p>Dawn Park</p>
+          </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="w3-row-padding">
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container w3-card">
+        <div class="w3-display-topleft w3-black w3-padding">R880 000</div>
+        <img src="img/property/Elands Rock Estate, Alberton North R880 000/pic1.jpg" alt="House" style="width:99%">
+          <div class="w3-container">
+              <p>Elands Rock Estate, Alberton North</p>
+          </div>
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container w3-card">
+        <div class="w3-display-topleft w3-black w3-padding">R680 000</div>
+        <img src="img/property/Ext 6, Vosloorus R680 000/pic1.jpg" alt="House" style="width:99%">
+          <div class="w3-container">
+              <p>Vosloorus, Extention 6</p>
+          </div>
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container w3-card">
+        <div class="w3-display-topleft w3-black w3-padding">R550 000</div>
+        <img src="img/property/Ext 7, Vosloorus R550 000/pic1.jpg" alt="House" style="width:99%">
+          <div class="w3-container">
+              <p>Vosloorus, Extention 7</p>
+          </div>
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container w3-card">
+        <div class="w3-display-topleft w3-black w3-padding">R950 000</div>
+        <img src="img/property/Klippoortjie R950 000/pic1.jpg" alt="House" style="width:99%">
+          <div class="w3-container">
+              <p>Klippoortjie</p>
+          </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- About Section -->
+  <div class="w3-container w3-padding-32" id="about">
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">About</h3>
+    <p>About Fiki Lunga Properties
+    </p>
+  </div>
+
+    <!-- Agents Section -->
+  <div class="w3-container w3-padding-32" id="agents">
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Agents</h3>
+      <div class="w3-row-padding w3-grayscale">
+          <div class="w3-col l3 m6 w3-margin-bottom">
+              <img src="img/agents/Fiki%20(Principal)%200721906817.jpg" alt="Fiki" style="width:100%; height: 300px">
+              <h3>Fiki</h3>
+              <p class="w3-opacity">CEO & Founder</p>
+              <p>About Fiki</p>
+              <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+          </div>
+          <div class="w3-col l3 m6 w3-margin-bottom">
+              <img src="img/agents/Mazet%20(agent)%200793117804.JPG" alt="Mazet" style="width:100%; height: 300px">
+              <h3>Mazet</h3>
+              <p class="w3-opacity">Senior Agent</p>
+              <p>About Mazet</p>
+              <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+          </div>
+          <div class="w3-col l3 m6 w3-margin-bottom">
+              <img src="img/agents/Mirriam%20(agent)%200731942240.jpeg" alt="Mirrian" style="width:100%; height: 300px">
+              <h3>Mirriam</h3>
+              <p class="w3-opacity">Senior Agent</p>
+              <p>About Mirriam</p>
+              <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+          </div>
+          <div class="w3-col l3 m6 w3-margin-bottom">
+              <img src="img/agents/Dudu%20(agent)%200844966561.jpeg" alt="Monique" style="width:100%; height: 300px">
+              <h3>Dudu</h3>
+              <p class="w3-opacity">Senior Agent</p>
+              <p>About Dudu</p>
+              <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+          </div>
+          <div class="w3-col l3 m6 w3-margin-bottom">
+              <img src="img/agents/Nthabi%20(agent)%200722048207.JPG" alt="Monique" style="width:100%; height: 300px">
+              <h3>Nthabi</h3>
+              <p class="w3-opacity">Senior Agent</p>
+              <p>About Nthabi</p>
+              <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+          </div>
+          <div class="w3-col l3 m6 w3-margin-bottom">
+              <img src="img/agents/Selinah%20(agent)%20060856591.jpeg" alt="Monique" style="width:100%; height: 300px">
+              <h3>Selinah</h3>
+              <p class="w3-opacity">Senior Agent</p>
+              <p>About Selinah</p>
+              <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+          </div>
+          <div class="w3-col l3 m6 w3-margin-bottom">
+              <img src="img/agents/Monique%20(Intern%20agent)%200792550713.jpeg" alt="Monique" style="width:100%; height: 300px">
+              <h3>Monique</h3>
+              <p class="w3-opacity">Intern</p>
+              <p>About Monique</p>
+              <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+          </div>
+      </div>
+  </div>
+
+
+  <!-- Contact Section -->
+  <div class="w3-container w3-padding-32" id="contact">
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Contact</h3>
+    <p>Lets get in touch and talk about your and our next project.</p>
+    <form action="/action_page.php" target="_blank">
+      <input class="w3-input" type="text" placeholder="Name" required name="Name">
+      <input class="w3-input w3-section" type="text" placeholder="Email" required name="Email">
+      <input class="w3-input w3-section" type="text" placeholder="Subject" required name="Subject">
+      <input class="w3-input w3-section" type="text" placeholder="Comment" required name="Comment">
+      <button class="w3-button w3-black w3-section" type="submit">
+        <i class="fa fa-paper-plane"></i> SEND MESSAGE
+      </button>
+    </form>
+  </div>
+  
+<!-- End page content -->
+</div>
+
+<!-- Google Map -->
+<!--<div id="googleMap" class="w3-grayscale" style="width:100%;height:450px;"></div>-->
+
+<!-- Footer -->
+<footer class="w3-center w3-black w3-padding-16">
+    <a href="#home" class="w3-button w3-light-grey"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
+    <div class="w3-xlarge w3-section">
+        <!--    <i class="fa fa-facebook-official w3-hover-opacity"></i>-->
+        <!--    <i class="fa fa-instagram w3-hover-opacity"></i>-->
+        <!--    <i class="fa fa-snapchat w3-hover-opacity"></i>-->
+        <!--    <i class="fa fa-pinterest-p w3-hover-opacity"></i>-->
+        <!--    <i class="fa fa-twitter w3-hover-opacity"></i>-->
+        <!--    <i class="fa fa-linkedin w3-hover-opacity"></i>-->
+
+        <span style="font-size: 12px;">Copyright &copy; <?= date('Y'); ?> Fiki Lunga Properties. All Rights Reserved.</span><br>
+        <a href="#" id="developer"><i><b>Developed by SnM ICT Systems</b></i></a>
+
+    </div>
+<!--  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">w3.css</a></p>-->
+</footer>
+
+<!-- Add Google Maps -->
+<script>
+    var myIndex = 0;
+    carousel();
+
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        myIndex++;
+        if (myIndex > x.length) {myIndex = 1}
+        x[myIndex-1].style.display = "block";
+        setTimeout(carousel, 10000);
+    }
+
+function myMap()
+{
+  myCenter=new google.maps.LatLng(41.878114, -87.629798);
+  var mapOptions= {
+    center:myCenter,
+    zoom:12, scrollwheel: false, draggable: false,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myCenter,
+  });
+  marker.setMap(map);
+}
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU&callback=myMap"></script>
+<!--
+To use this code on your website, get a free API key from Google.
+Read more at: https://www.w3schools.com/graphics/google_maps_basic.asp
+-->
+
+</body>
+</html>
